98222a3323afd8549da8cc8814a3f46d287f806e
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2023-11-20 15:52:14 -0800

    Reduced seeded tags from 5 to 4

--- dev/models/index.ts
+++ dev/models/index.ts
@@ -4,6 +4,6 @@ import { trail,trailSchema } from './trails.js';
 import { user } from './user.js';
 import { comment } from './comment.js';
 import { location } from './location.js';
+import photo from './photos.js'
 
-
-export {trail,user,comment,location,trailSchema}
+export {trail,user,comment,location,trailSchema,photo}
