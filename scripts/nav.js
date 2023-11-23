449bf430eb21c7bf815759e802881687aafed77b
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2023-11-23 08:00:24 -0800

    load trails on scroll added

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,29 @@
 "use strict";
-const btn = document.querySelector('.mobile-menu-button');
-const menu = document.querySelector('.mobile-menu');
+const btn = document.querySelector('.mobile-menu-button'), menu = document.querySelector('.mobile-menu');
 btn?.addEventListener('click', () => {
     menu?.classList.toggle('hidden');
 });
+const theWholePage = document.querySelector('#allDiv'), allDivs = document.querySelectorAll('.singleTrail');
+console.log(allDivs);
+let locationNumber = 9, firstDiv = 15, endDiv = 25;
+const loadDivs = (initial, max) => {
+    for (let index = initial; index < max; index++) {
+        let singleDiv = allDivs[index];
+        singleDiv.classList.toggle('hidden');
+        singleDiv.classList.toggle('flex');
+    }
+}, onLoadLoadDivs = () => {
+    window.addEventListener('DOMContentLoaded', () => {
+        loadDivs(0, 15);
+    });
+}, loadMore = () => {
+    let locationCheckDiv = allDivs[locationNumber];
+    if (locationCheckDiv.getBoundingClientRect().y <= 0) {
+        loadDivs(firstDiv, endDiv);
+        firstDiv += 10;
+        endDiv += 10;
+        locationNumber += 10;
+    }
+};
+onLoadLoadDivs();
+document.addEventListener('scroll', loadMore);
